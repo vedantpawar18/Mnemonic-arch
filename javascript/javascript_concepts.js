871362906cb75fd341a9d13bd9c4ea7c0f86aa50
Vedant Pawar <101568818+vedantpawar18@users.noreply.github.com>
2025-03-20 12:22:53 +0530

    Update javascript_concepts.js

--- javascript/javascript_concepts.js
+++ javascript/javascript_concepts.js
@@ -1,2 +1,3 @@
 https://github.com/xavierdaniel1997/javascript-interview-questions
 
+https://www.linkedin.com/feed/update/urn:li:activity:7297110026739929089/?updateEntityUrn=urn%3Ali%3Afs_updateV2%3A%28urn%3Ali%3Aactivity%3A7297110026739929089%2CFEED_DETAIL%2CEMPTY%2CDEFAULT%2Cfalse%29
