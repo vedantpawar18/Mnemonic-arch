c0275ad2c1a02f30d1b39b10a2c3d0fcaef5864b
Vedant Pawar <101568818+vedantpawar18@users.noreply.github.com>
2025-03-20 12:20:29 +0530

    Update javascript_concepts.js

--- javascript/javascript_concepts.js
+++ javascript/javascript_concepts.js
@@ -1,13 +1,2 @@
-/* Question: What is a closure in JavaScript?
-   Answer: A function inside another function that remembers the outer function's variables.
-   Explanation: Imagine you have a backpack with cookies. You put your hand inside to grab a cookie. Your hand (inner function) still knows the cookies (outer variables) exist, even if you close the bag! */
-function outer() {
-    let count = 0;
-    return function inner() {
-        count++;
-        console.log(count);
-    };
-}
-const counter = outer();
-counter(); // 1
-counter(); // 2
+https://github.com/xavierdaniel1997/javascript-interview-questions
+
